28e8e223a21bef17cd1a6d19ee227804fef7ed31
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-01-22 08:04:08 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+"use strict";
 
+const loginFormEl = document.querySelector("form.login-form");
+// const loginFormSubmitEl = document.querySelector('form.login-form > button');
+
+loginFormEl.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm (event) {
+    event.preventDefault();
+    const {elements: { email, password }} = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        alert("Все поля должны быть заполнены.")
+    } else {
+        console.log(email.value);
+        console.log(password.value);
+        const submitForm = {
+            email: email.value,
+            password: password.value,
+        };
+        event.currentTarget.reset();
+        return submitForm;
+    }
+    
+}
+console.log(submitForm);
